9a77e7b32d370f21a60ed9908a2de60622696a8b
Ferrasz <111441434+FerraszXD@users.noreply.github.com>
2022-08-17 19:38:54 -0300

    Update banco.sql

--- banco.sql
+++ banco.sql
@@ -6,7 +6,7 @@ CREATE TABLE receitas(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     titulo VARCHAR(255) NOT NULL,
     modo_preparo TEXT NOT NULL,
-    autor VARCHAR(255) NOT NULL
-    ingrediente TEXT NOT NULL,
-    equipamento TEXT NOT NULL,
+    autor VARCHAR(255) NOT NULL,
+    ingredientes TEXT NOT NULL,
+    equipamentos TEXT NOT NULL
 );
