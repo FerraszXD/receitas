e8cc0ec85f82d33aab9475e6ef3549f11b9e31cc
Ferrasz <111441434+FerraszXD@users.noreply.github.com>
2022-08-17 11:36:40 -0300

    Create cadastro.php

---
+++ cadastro.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="estilo.css">
+        <title>Coktop - as melhores receitas</title>
+    </head>
+    <body>
+        <h1>Coktop - as melhores receitas</h1>
+        <h4>Cadastro de receitas</h4>
+        <form action="salvar_reiceita.php" method="POST">
+            <div>
+                <label for="titulo">Titulo</label>
+                <input type="text" name="titulo" id="titulo">
+            </div>
+            <div>
+                <label for="ingredientes">Ingredientes</label>
+                <textarea name="ingredientes" id="ingredientes" cols="5"></textarea>
+            </div>
+            <div>
+                <label for="autor">Autor</label>
+                <input type="text" name="autor" id="autor">
+            </div>
+            <div>
+                <label for="modo_de_preparo">Modo De Preparo</label>
+                <input type="text" name="modo_de_preparo" id="modo_de_preparo">
+            </div>
+            <div>
+                <label for="equipamentos">Equipamentos</label>
+                <input type="text" name="equipamentos" id="equipamentos">
+            </div>
+            <input type="submit" value="Salvar">
+        </form>
+    </body>
+</html>
