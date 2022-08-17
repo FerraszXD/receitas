119d0a7c0e62d939d947fa3ab35fd0cf5b17dc0f
Ferrasz <111441434+FerraszXD@users.noreply.github.com>
2022-08-17 11:37:23 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="estilo.css">
+        <title>Coktop - as melhores receitas</title>
+    </head>
+    <body>
+        <h1>Coktop - as melhores receitas</h1>
+        <a href="cadastro_receita.php">Cadastrar nova Receita</a>
+    </body>
+</html>
