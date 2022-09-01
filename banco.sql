7ebd1cbc7839f96e1d7148a7ee48d8bae429e8e5
Ferrasz <111441434+FerraszXD@users.noreply.github.com>
2022-09-01 19:39:36 -0300

    Update banco.sql

--- banco.sql
+++ banco.sql
@@ -5,8 +5,9 @@ USE Coktop;
 CREATE TABLE receitas(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     titulo VARCHAR(255) NOT NULL,
-    modo_preparo TEXT NOT NULL,
+    modo_de_preparo TEXT NOT NULL,
     autor VARCHAR(255) NOT NULL,
     ingredientes TEXT NOT NULL,
     equipamentos TEXT NOT NULL
 );
+
